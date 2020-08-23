7c2691d7750b548ed5ef1febcb8186f609797a50
yugsalvation <33853459+yugsalvation@users.noreply.github.com>
2020-08-23 11:31:11 +0530

    Updates

---
+++ precache-manifest.b9c0b2414819e3eea6a0015d0d9528b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee3f93d2f5213482aab16838d5bc833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a50f1b6de600497b72e9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2afc886076e982fc7236",
+    "url": "/static/js/2.8ae71109.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ae71109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50f1b6de600497b72e9",
+    "url": "/static/js/main.ce56d34a.chunk.js"
+  },
+  {
+    "revision": "be12a67d60b2a122d91d",
+    "url": "/static/js/runtime-main.548fa58d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
